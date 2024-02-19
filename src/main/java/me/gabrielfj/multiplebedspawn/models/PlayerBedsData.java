615c6cbabe0927b45eaad9587a58c339da06d1fe
Drunk Russian Gun <32940382+DrunkRussianGun@users.noreply.github.com>
2024-02-19 19:23:42 +0400

    Fixed /sharebed command completely (#28)
    * Fixed incorrect removal of bed UUID on bed sharing
    
    We don't need to remove bed UUID when it gets shared since the bed
    itself is still owned by the receiver player.
    
    * Fixed wrong teleport on shared bed
    
    * Reuse the already registered bed instead of creating a new one
    
    This allows to keep both bed name and its spawn location untouched.
    
    * Bump version to 1.9.2

--- src/main/java/me/gabrielfj/multiplebedspawn/models/PlayerBedsData.java
+++ src/main/java/me/gabrielfj/multiplebedspawn/models/PlayerBedsData.java
@@ -7,8 +7,12 @@ import java.io.Serializable;
 import java.util.HashMap;
 
 public class PlayerBedsData implements Serializable {
+    private static final long serialVersionUID = 6158573570409965948L;
 
     private HashMap<String, BedData> bedData = new HashMap<String, BedData>();;
+
+    public PlayerBedsData() {
+    }
 
     public PlayerBedsData(Player p, Block bed, String bedUUID){
         BedData tempBedData = new BedData(bed, p);
@@ -18,6 +22,11 @@ public class PlayerBedsData implements Serializable {
     public void setNewBed(Player p, Block bed, String bedUUID){
         BedData tempBedData = new BedData(bed, p);
         this.bedData.put(bedUUID, tempBedData);
+    }
+
+    public void shareBed(PlayerBedsData receiverPlayerBedsData, String bedUUID) {
+        BedData bedToShare = bedData.remove(bedUUID);
+        receiverPlayerBedsData.bedData.put(bedUUID, bedToShare);
     }
 
     public void removeBed(String bedUUID){
